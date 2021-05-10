7d0fb9344729af5a64d9ef64775606a97283c22a
Prakash <akash2046@gmail.com>
2021-05-10 13:35:56 +0545

    conflict resolve 3

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -2,8 +2,4 @@ import $ from 'jquery';
 
 import './component/navigation';
 import './component/mobilemenu';
-<<<<<<< HEAD
-import './component/slider';
-=======
 import './component/scrollnavigation';
->>>>>>> 066d7719067e3401048557746b27ce0ac7d0d952

fcbd93d816f496d017526c4f4bd3191266a5647b
Prakash <akash2046@gmail.com>
2021-05-18 17:41:52 +0545

    reseller program added

--- templates/tpl-reseller-program.php
+++ templates/tpl-reseller-program.php
@@ -1,110 +1,128 @@
 <?php
 /* Template Name: Reseller Program */ 
 
-get_header();?>
+get_header();
+?>
+
+<?php
+	$banner_image_url = '';
+	$banner_image     = get_field( 'reseller_program_bg_image' );
+	if ( ! empty( $banner_image ) ) {
+		$banner_image_url = $banner_image['url'];
+	}
+?>
 
 
 <main id="main" class="site-main">
-	<section class="page-banner reseller-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/reseller-banner-bg.png'; ?>')">
+	<section class="page-banner reseller-page-banner" style="background-image:url('<?php echo $banner_image_url; ?>')">
 		<div class="wrapper">
 			<div class="page-banner-content">
-				<h1 class="large-banner__heading">Reseller Program</h1>
-				<p>We Exclusively Recomend Reseller</p>
-				<a href="" class="btn--white">Learn More <i class="fas fa-arrow-right"></i></a>
+				<h1 class="large-banner__heading"><?php the_field( 'reseller_program_title' ); ?></h1>
+				<p><?php the_field( 'reseller_program_sub_title' ); ?></p>
+				<a href="<?php the_field( 'reseller_button_link' ); ?>" class="btn--white"><?php the_field( 'reseller_button' ); ?><i class="fas fa-arrow-right"></i></a>
 			</div>
 		</div>
 	</section>
 	
 	<section class="reseller-exclusive page-section">
 		<div class="wrapper">
-			<h2 class="section-header__title">Our Exclusive Reseller Program</h2>
+			<h2 class="section-header__title"><?php the_field( 'exclusive_reseller_title' ); ?></h2>
 			<span class="section-header__decoration-element"></span>
-			<h4 class="section-header__Orangesub-title">Only work with the best in the industry! Join our reseller program Today.</h4>
-			<p>Our program is designed to help you become successful. With our great datacenter locations and rock-solid infrastructure, you will join and work with the best.</p>
+			<h4 class="section-header__Orangesub-title"><?php the_field( 'exclusive_reseller_sub_title' ); ?></h4>
+			<p><?php the_field( 'exclusive_reseller_short_detail' ); ?></p>
 		</div>
 	</section>
-	
-	<section class="reseller-program-work page-section" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/program-work-bg.png'; ?>')">
+	<?php
+	$reseller_bg_image = '';
+	$bg_image     = get_field( 'how_reseller_works_bg_image' );
+	if ( ! empty( $bg_image ) ) {
+		$reseller_bg_image = $bg_image['url'];
+	}
+	?>
+	<section class="reseller-program-work page-section" style="background-image:url('<?php echo $reseller_bg_image; ?>')">
 		<div class="wrapper">
-			<h2 class="section-header__title">How Does Dedicated Solutions Reseller Program It Work?</h2>
+			<h2 class="section-header__title"><?php the_field( 'reseller_program_works_title' ); ?></h2>
 			<span class="section-header__decoration-element--dark"></span>
 			<div class="pogram-work-grid">
+			<?php if ( have_rows( 'reseller_package_list' ) ) : ?>
 				<div class="rows rows--gutters-small">
-					<div class="rows__medium-3">
-						<div class="product-work-card card-1">
+					<?php
+					$counter_card = 1;
+					while ( have_rows( 'reseller_package_list' ) ) :
+						the_row();
+
+						$package_icon = '';
+						$product_icon     = get_sub_field( 'product_icon' );
+						if ( ! empty( $product_icon ) ) {
+							$package_icon = $product_icon['url'];
+						}
+
+						?>
+						<div class="rows__medium-3">
+						<div class="product-work-card card-<?php echo $counter_card;?>">
 							<div class="product-icon-box">
-								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/server-icon-1.png'; ?>" alt="server-icon-1">
+								<img src="<?php echo $package_icon; ?>" alt="server-icon-1">
 							</div>
-							<h6>3-9</h6>
-							<h3>Products</h3>
-							<div class="rate">5%</div>
-							<h4>No Set Up Fees</h4>
+							<h6><?php the_sub_field( 'size_label' ); ?></h6>
+							<h3><?php the_sub_field( 'reseller_product_title' ); ?></h3>
+							<div class="rate"><?php the_sub_field( 'offer_percent' ); ?></div>
+							<h4><?php the_sub_field( 'set_up_fee' ); ?></h4>
 						</div>
-					</div>
-					<div class="rows__medium-3">
-						<div class="product-work-card card-2">
-							<div class="product-icon-box">
-								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/server-icon-2.png'; ?>" alt="server-icon-2">
-							</div>
-							<h6>10-19</h6>
-							<h3>Dedicated Servers</h3>
-							<div class="rate">10%</div>
-							<h4>No Set Up Fees</h4> 
-						</div>
-					</div>
-					<div class="rows__medium-3">
-						<div class="product-work-card card-3">
-							<div class="product-icon-box">
-								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/server-icon-3.png'; ?>" alt="server-icon-3">
-							</div>
-							<h6>20-99</h6>
-							<h3>Dedicated Servers</h3>
-							<div class="rate">15%</div>
-							<h4>No Set Up Fees</h4>  
-						</div>
-					</div>
-					<div class="rows__medium-3">
-						<div class="product-work-card card-4">
-							<div class="product-icon-box">
-								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/server-icon-4.png'; ?>" alt="server-icon-4">
-							</div>
-							<h6>99 or More</h6>
-							<h3>Dedicated Servers</h3>
-							<div class="rate">20%</div>
-							<h4>No Set Up Fees</h4> 
-						</div>
-					</div>
+						</div>					
+					<?php 
+					$counter_card++;
+					endwhile; ?>
 				</div>
+			<?php endif; ?>
 			</div>
-			<a href="" class="btn--white">Contact Us <i class="fas fa-arrow-right"></i></a>
+			<a href="<?php the_field( 'contact_link' ); ?>" class="btn--white"><?php the_field( 'contact_label' ); ?> <i class="fas fa-arrow-right"></i></a>
 		</div>
 	</section>
 
 	<section class="why-choose-dedicatedSolutions page-section">
 		<div class="wrapper">
-			<h2 class="section-header__title">Why Choose Our Dedicated Solutions Reseller Program?</h2>
+			<h2 class="section-header__title"><?php the_field('why_choose_our_dedicated_title'); ?></h2>
 			<span class="section-header__decoration-element"></span>
-			<h4 class="section-header__Orangesub-title">A worldwide leader in reseller program offer over 800 products</h4>
+			<h4 class="section-header__Orangesub-title"><?php the_field( 'why_choose_our_dedicated_sub_title' ); ?></h4>
 			<div class="why-choose-grid">
-				<div class="rows rows--gutters-small">
+				<div class="rows rows--gutters-small">					
+					<?php
+					$counter_why_feature = 1;
+					if ( have_rows( 'why_choose_features' ) ) : 
+					while ( have_rows( 'why_choose_features' ) ) :
+					the_row();
+
+					$reseller_image = '';
+					$feature_block_image     = get_sub_field( 'feature_block_image' );
+					if ( ! empty( $feature_block_image ) ) {
+						$reseller_image = $feature_block_image['url'];
+					}
+					?>
 					<div class="rows__medium-4">
-						<div class="why-choose-card">
+						<div class="why-choose-card">						   
 							<figure class="thumbnail-image">
-								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/why-choose-img1.jpg'; ?>" alt="why-choose-img1">
+								<img src="<?php echo $reseller_image; ?>" alt="<?php the_sub_field( 'feature_title' ); ?>">
 							</figure>
-							<div class="why-choose-card-content">
-								<h2>Offer the Best Products</h2>
+							<div class="why-choose-card-content">						
+								<h2><?php the_sub_field( 'feature_title' ); ?></h2>
 								<ul>
-									<li>Offer Tier III and Tier IV Datacenters!</li>
-									<li>No downtime since 5 years.</li>
-									<li>SSAE16 & PCI conform.</li>
-									<li>Over 100 network carriers.</li>
-									<li>Up to Unlimited Bandwidth.</li>
-								</ul>
+								<?php															
+								while ( have_rows( 'features_lists' ) ) : the_row();								
+								?>
+								<li><?php the_sub_field( 'list' ); ?></li>
+								<?php 
+								endwhile;
+								?>
+								</ul> 							
 							</div>
 						</div>
 					</div>
-					<div class="rows__medium-4">
+					<?php 
+					$counter_why_feature++;
+					endwhile; 
+					endif;
+					?>
+					<!-- <div class="rows__medium-4">
 						<div class="why-choose-card">
 							<figure class="thumbnail-image">
 								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/why-choose-img2.jpg'; ?>" alt="why-choose-img2">
@@ -136,7 +154,7 @@ get_header();?>
 								</ul>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 			</div>
 		</div>

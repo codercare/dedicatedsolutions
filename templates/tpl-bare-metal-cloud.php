5242e7908f73a92ec2aa86df1d7137ff1f17cc9c
Prakash <akash2046@gmail.com>
2021-05-21 21:35:47 +0545

    template on demand storage

--- templates/tpl-bare-metal-cloud.php
+++ templates/tpl-bare-metal-cloud.php
@@ -1,15 +1,19 @@
 <?php
-/* Template Name: About */ 
+/* Template Name: Bare Metal Cloud */ 
 
 get_header();?>
 
 
-
-<section class="client-showcase">
-	<div class="wrapper">
-	<h1>Bare Metal Cloud </h1>
-	</div>
-</section>
+<main id="site-main" class="site-main">
+	<section class="page-banner about-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
+		<div class="wrapper">
+			<div class="page-banner-content">
+				<h1 class="large-banner__heading">Bare Metal Cloud</h1>
+				<p>Get the performance of bare metal with cloud</p>				
+			</div>
+		</div>
+	</section>
+</main>
 
 
 <?php get_footer(); ?>
--- templates/tpl-plesk-server-management-tool.php
+++ templates/tpl-plesk-server-management-tool.php
@@ -4,12 +4,15 @@
 get_header();?>
 
 
-
-<section class="client-showcase">
-	<div class="wrapper">
-	<h1>Plesk Server Management Tool</h1>
-	</div>
-</section>
-
+<main id="site-main" class="site-main">
+	<section class="page-banner about-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
+		<div class="wrapper">
+			<div class="page-banner-content">
+				<h1 class="large-banner__heading">Plesk Server Management Tool</h1>
+				<p>Simple, Secure, and Reliable Website and Web Server Management Tool</p>				
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>

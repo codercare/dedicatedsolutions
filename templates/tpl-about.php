d4024a2ba13114534a03bb8f4b2e3bac5aa123a1
Prakash <akash2046@gmail.com>
2021-05-21 16:08:58 +0545

    space removed

--- templates/tpl-about.php
+++ templates/tpl-about.php
@@ -3,21 +3,15 @@
 
 get_header();?>
 
-<?php
-	$banner_image_url = '';
-	$banner_image     = get_field( 'about_dedicated_solutions_banner' );
-	if ( ! empty( $banner_image ) ) {
-		$banner_image_url = $banner_image['url'];
-	}
-?>
+
 
 <main id="site-main" class="site-main">
-	<section class="page-banner about-page-banner" style="background-image:url('<?php echo $banner_image_url; ?>')">
+	<section class="page-banner about-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
 		<div class="wrapper">
 			<div class="page-banner-content">
-				<h1 class="large-banner__heading"><?php the_field( 'about_dedicated_title' ); ?></h1>
-				<p><?php the_field( 'about_dedicated_sub_title' ); ?></p>
-				<a href="<?php the_field( 'about_dedicated_button_link' ); ?>" class="btn--orange"><?php the_field( 'about_dedicated_button_label' ); ?> <i class="fas fa-arrow-right"></i></a>
+				<h1 class="large-banner__heading">About Dedicated Solutions</h1>
+				<p>Our mission is to deliver top-notch dedicated server solutions!</p>
+				<a href="" class="btn--orange">Learn More <i class="fas fa-arrow-right"></i></a>
 			</div>
 		</div>
 	</section>
@@ -25,26 +19,23 @@ get_header();?>
 	<section class="welcom-section page-section">
 		<div class="wrapper">
 			<div class="section-header u-txt-center">
-				<h2 class="section-header__title"><?php the_field( 'weclome_block_title' ); ?></h2>
+				<h2 class="section-header__title">Welcome to DedicatedSolutions.com</h2>
 				<span class="section-header__decoration-element"></span>
-				<p class="section-header__sub-title"><?php the_field( 'weclome_block_sub_title' ); ?></p>
+				<p class="section-header__sub-title">Our Goals / Vision and Mission</p>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
 					<figure class="thumbnail-img">
-						<?php
-						$block_image = '';
-						$b_image     = get_field( 'weclome_block_image' );
-						if ( ! empty( $b_image ) ) {
-							$block_image = $b_image['url'];
-						}
-						?>
-						<img src="<?php echo $block_image; ?>" alt="<?php the_field( 'weclome_block_title' ); ?>">
+						<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/welcome-thumbnail.jpg'; ?>" alt="welcome-thumbnail">
 					</figure>
 				</div>
 				<div class="col-md-8">
 					<div class="welcome-content">
-						<?php the_field( 'weclome_block_content' ); ?> 
+						<p>Our goals are clearly defined! We want to deliver the best service to our customers while keeping the costs for our customers as low as possible.</p>
+						<p>Through the combination of Tier III & Tier IV data center facilities, the ability to offer our customers multiple 10 Gig pipes at each location and only providing the best hardware possible, we can truly offer a 99.9% uptime – guaranteed.</p>
+						<p>But it is not only the hardware that makes DedicatedSolutions.com a preferred home for dedicated servers, it is also the humans behind it. Our staff constantly strives to serve you with the best possible support. All our facilities have 24×7 staffing – whether it be network technicians, system administrators or support members.</p>
+						<p>We have set the bar high for us, but this is what we love and with our team of experts and professionals we are able to make it happen.</p>
+						<p>The mission for our future is to expand into as many countries as possible with our own operated data centers. This will ensure that our customers can have servers located nearly in every spot in the world – interconnected with each other on fast connections.</p>
 					</div>
 				</div>
 			</div>
@@ -52,24 +43,17 @@ get_header();?>
 	</section>
 
 	<section class="ceo-detail page-section" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/ceo-bg.jpg'; ?>')">
-		<div class="wrapper"> 
+		<div class="wrapper">
 			<div class="row">
 				<div class="col-md-5">
 					<figure class="thumbnal-img">
-					    <?php
-						$ceo_image = '';
-						$ceo_url     = get_field( 'ceo_image' );
-						if ( ! empty( $ceo_url ) ) {
-							$ceo_image = $ceo_url['url'];
-						}
-						?>
-						<img src="<?php echo $ceo_image; ?>" alt="<?php the_field( 'ceo_name' ); ?>">
+						<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/ceo-profile.jpg'; ?>" alt="ceo-profile">
 					</figure>
 				</div>
 				<div class="col-md-7">
 					<div class="ceo-content">
-						<h1><?php the_field( 'ceo_name' ); ?><span><?php the_field( 'designation_detail' ); ?></span></h1>
-						<p><?php the_field( 'ceo_short' ); ?></p>
+						<h1>Christoph Schmolmueller <span>CEO / Founder</span></h1>
+						<p>Christoph is the driving force here at DedicatedSolutions.com. His aim is to deliver outstanding dedicated server solutions at an affordable price.</p>
 					</div>
 				</div>
 			</div>
@@ -80,32 +64,42 @@ get_header();?>
 		<div class="wrapper">
 			<div class="quick-fact-inner-content">
 				<div class="section-header u-txt-center">
-					<h2 class="section-header__title">QUICK FACTS</h2>
+					<h2 class="section-header__title">Quick Facts</h2>
 					<span class="section-header__decoration-element"></span>
 				</div>
 				<div class="row">
-					<?php
-					if ( have_rows( 'quick_facts' ) ) : 
-					while ( have_rows( 'quick_facts' ) ) :
-						the_row();
-					$quickfact_image = '';
-					$quickfact_url     = get_sub_field( 'facts_icon' );
-					if ( ! empty( $quickfact_url ) ) {
-						$quickfact_image = $quickfact_url['url'];
-					}
-					?>
 					<div class="col-md-6">
 						<div class="quick-fact-block">
 							<div class="quick-fact-icon">
-								<img src="<?php echo $quickfact_image; ?>" alt="<?php the_sub_field( 'facts_label' ); ?>">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/quick-fact-icon1.png'; ?>" alt="quick-fact-icon1">
 							</div>
-							<h3><?php the_sub_field( 'facts_label' ); ?></h3>
+							<h3>Self Service Control Panel</h3>
 						</div>
 					</div>
-					<?php 
-					endwhile;
-					endif;
-					?>
+					<div class="col-md-6">
+						<div class="quick-fact-block">
+							<div class="quick-fact-icon">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/quick-fact-icon2.png'; ?>" alt="quick-fact-icon2">
+							</div>
+							<h3>Awesome Guarantees</h3>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="quick-fact-block">
+							<div class="quick-fact-icon">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/quick-fact-icon3.png'; ?>" alt="quick-fact-icon3">
+							</div>
+							<h3>5 World-wide Office Locations</h3>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="quick-fact-block">
+							<div class="quick-fact-icon">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/quick-fact-icon3.png'; ?>" alt="quick-fact-icon3">
+							</div>
+							<h3>2 Datacenter Locations (2 more coming soon)</h3>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -114,20 +108,11 @@ get_header();?>
 	<section class="our-locations">
 		<div class="wrapper">
 			<div class="section-header u-txt-center">
-				<h2 class="section-header__title"><?php the_field( 'our_location_title' ); ?></h2>
+				<h2 class="section-header__title">Our Locations World-Wide</h2>
 				<span class="section-header__decoration-element"></span>
 			</div>
 			<figure class="thumbnail-img">
-				<?php 
-				$location_map = ''; 
-				$location_image     = get_field( 'our_location_image' );
-				if ( ! empty( $location_image ) ) {
-					$location_map = $location_image['url'];
-				}
-				
-				?>
-				<img src="<?php echo $location_map; ?>" alt="location-map">
-				<!-- <img src="<?php echo get_template_directory_uri() . '/dist/assets/images/location-map.png'; ?>" alt="location-map"> -->
+				<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/location-map.png'; ?>" alt="location-map">
 			</figure>
 			<div class="location-detail">
 				<ul>
@@ -159,11 +144,11 @@ get_header();?>
 	<section class="speak-with-expert">
 		<div class="wrapper">
 			<div class="section-header u-txt-center">
-				<h2 class="section-header__title"><?php the_field( 'speak_with_expert_title' ); ?></h2>
+				<h2 class="section-header__title">Speak With An Expert</h2>
 				<span class="section-header__decoration-element"></span>
-				<p class="section-header__sub-title"><?php the_field( 'speak_with_expert_detail' ); ?></p>
+				<p class="section-header__sub-title">Need help deciding on the best server for your needs? Speake with an expert now!</p>
 			</div>
-			<a href="<?php the_field( 'speak_with_expert_button_link' ); ?>" class="btn--orange"><?php the_field( 'speak_with_expert_button' ); ?></a>
+			<a href="" class="btn--orange">How we can help you</a>
 		</div>
 	</section>
 </main>
--- templates/tpl-plesk-server-management-tool.php
+++ templates/tpl-plesk-server-management-tool.php
@@ -2,14 +2,14 @@
 /* Template Name: Plesk Server Management Tool */ 
 
 get_header();?>
-
-
-
-<section class="client-showcase">
-	<div class="wrapper">
-	<h1>Plesk Server Management Tool</h1>
-	</div>
-</section>
-
-
+<main id="site-main" class="site-main">
+	<section class="page-banner about-page-banner" style="background-image:url('<?php echo $banner_image_url; ?>')">
+		<div class="wrapper">
+			<div class="page-banner-content">
+				<h1 class="large-banner__heading">Plesk Server Management Tool</h1>
+				<p>Simple, Secure, and Reliable Website and Web Server Management Tool</p>				
+			</div>
+		</div>
+	</section>
+</main>
 <?php get_footer(); ?>

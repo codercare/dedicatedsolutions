f8254b363dc8a0388bf49d8277d6e90d35827d75
saroj sharma <srmasharad49@gmail.com>
2021-05-23 16:52:21 +0545

    Started Custon Solutions page design

--- templates/tpl-custom-solutions.php
+++ templates/tpl-custom-solutions.php
@@ -3,13 +3,18 @@
 
 get_header();?>
 
-
-
-<section class="client-showcase">
-	<div class="wrapper">
-	<h1>Custom Solutions</h1>
-	</div>
-</section>
-
+<main id="site-main" class="site-main">
+	<section class="page-banner customSol-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
+		<div class="wrapper">
+			<div class="page-banner-content">
+				<h1 class="large-banner__heading">Custom Solutions</h1>
+				<p>Tell us what you need and we’ll build it.</p>
+				<a href="" class="btn--orange">Custom Solutions Overview</a>
+				<a href="" class="btn--white">Premium Features</a>
+			</div>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
+

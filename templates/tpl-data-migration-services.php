7cc805dfed55ce179e84213a3638905435267513
saroj sharma <srmasharad49@gmail.com>
2021-06-07 19:13:05 +0545

    Working on Managed Colocation and Data Migration Services pages

--- templates/tpl-data-migration-services.php
+++ templates/tpl-data-migration-services.php
@@ -4,8 +4,8 @@
 get_header();?>
 
 
-<main id="site-main" class="site-main">
-	<section class="page-banner about-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
+<main id="fullpage" class="site-main">
+	<section class="page-banner about-page-banner dm-service-banner section fp-auto-height" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/about-banner.jpg'; ?>')">
 		<div class="wrapper">
 			<div class="page-banner-content">
 				<h1 class="large-banner__heading">Data Migration Services</h1>
@@ -13,7 +13,107 @@ get_header();?>
 				<a href="" class="btn--orange">Learn More <i class="fas fa-arrow-right"></i></a>
 			</div>
 		</div>
+	</section>
+
+	<section class="data-migration-simple section fp-auto-height" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/data-migration-simple-img.png'; ?>')">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="section-header">
+						<h2 class="section-header__title">Data Migration is Simple</h2>
+						<span class="section-header__decoration-element--dark"></span>
+						<h4 class="section-header__Orangesub-title">We understand your wish to move your hosted environment, but worry about where to start. It is really simple. Our experts can help you migrating servers, applications, dedicated and virtual environments, and emails to our single/cluster servers without disturbing the environment.</h4>
+					</div>
+					<div class="data-migration-block-wrap">
+						<div class="data-migration-block">
+							<div class="data-migration-icon-box migrating">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/migrating.png'; ?>" alt="migrating">
+							</div>
+							<h2>cPanel & Plesk Migration</h2>
+							<p>We guarantee without or minimum downtime for your cPanel/Plesk powered environment migration to our infrastructure. It’s a free service. We have a well defined process and tools for faster migration of your web files.</p>
+						</div>
+						<div class="data-migration-block">
+							<div class="data-migration-icon-box data-storage">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/data-storage.png'; ?>" alt="data-storage">
+							</div>
+							<h2>Complex Data Migration and transition Services</h2>
+							<p>Dedicated Solutions is capable of moving any environment safely to our infrastructure. Our team will help you moving your private cloud environment, applications, domain, and databases. We have a proven track record of cross-platform migration as well.</p>
+						</div>
+						<div class="data-migration-block">
+							<div class="data-migration-icon-box migration">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/Migration.png'; ?>" alt="Migration">
+							</div>
+							<h2>Custom Migrations</h2>
+							<p>We provide guaranteed custom migration for all environments. Our team is able to move your web or mail servers to our infrastructure even without control panels. Our well documented and tested process methods and tools are helping us to provide flawless custom migration</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>	
+
+	<section class="what-can-we-migrate section">
+		<div class="wrapper">
+			<div class="section-header u-txt-center">
+				<h2 class="section-header__title">What Can We Migrate?</h2>
+				<span class="section-header__decoration-element"></span>
+				<h4 class="section-header__Orangesub-title">Virtually everything! Our migration specialists are experts in migrating Cloud, Multiple sites, Servers, Applications, Email servers, Single website, and Enterprise cloud infrastructure. DedicatedSolutions is providing pre migration assessment service as well.</h4>
+			</div>
+			<div class="wcw-migrate-block-wrap">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="wcw-migrate-block">
+							<div class="wcw-icon-box">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/server.png'; ?>" alt="server">
+							</div>
+							<h3>Servers</h3>
+							<ul class="check-list-group orange-check">
+								<li>Move your dedicated /virtual servers, and cloud hosting infrastructure to our sophisticated single servers, Database Clusters, and dedicated servers.</li>
+								<li>Upgrade your OS. Move images, apps, and data.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="wcw-migrate-block">
+							<div class="wcw-icon-box">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/application.png'; ?>" alt="application">
+							</div>
+							<h3>Applications</h3>
+							<ul class="check-list-group orange-check">
+								<li>We help you moving application platform. For eg: from Drupal to WordPress or contrariwise.</li>
+								<li>Migrate your Content Management Systems (CMS), Software as a Service (SaaS) apps, or custom apps to DedicatedSolutions</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="wcw-migrate-block">
+							<div class="wcw-icon-box">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/databases.png'; ?>" alt="databases">
+							</div>
+							<h3>Databases</h3>
+							<ul class="check-list-group orange-check">
+								<li>We provide MySQL, SQL, Oracle, PostgreSQL, Hadoop, and noSQL database hosting service.</li>
+								<li>Move or upgrade your database. Change from SQL to MySQL or the other way around.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="wcw-migrate-block">
+							<div class="wcw-icon-box">
+								<img src="<?php echo get_template_directory_uri() . '/dist/assets/images/email-1.png'; ?>" alt="email-1">
+							</div>
+							<h3>Email</h3>
+							<ul class="check-list-group orange-check">
+								<li>We have cost effective email solutions such as HostedExchange®for you.</li>
+								<li>Exchange Hybrid email offerings of DedicatedSolutions is intended to give you best email experience.</li>
+								<li>Use the best and experience the difference!</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
 
 

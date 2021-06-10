7fe27065934eb50deebd66af829d444db47607c5
Prakash <akash2046@gmail.com>
2021-06-10 22:01:15 +0545

    bare metal cloud

--- templates/BK/bk-bare-metal-cloud.php
+++ templates/BK/bk-bare-metal-cloud.php
@@ -1,8 +1,9 @@
 <?php
-/* Template Name: Test2 */ 
+/*  Bare Metal Cloud */ 
 
 get_header();?>
-?>
+
+
 <main id="fullpage" class="site-main">
 	<section class="section">
 		<section class="page-banner about-page-banner bmc-page-banner" style="background-image:url('<?php echo get_template_directory_uri() . '/dist/assets/images/bare-metal-cloud-banner.jpg'; ?>')">
@@ -560,4 +561,6 @@ get_header();?>
 		</div>
 	</section>
 </main>
+
+
 <?php get_footer(); ?>
